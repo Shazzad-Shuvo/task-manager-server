ffce47528c4dde55329bbcaa28500df9877da8e5
Shazzad-Shuvo <md.rahman.shazzadur@gmail.com>
2023-12-25 12:58:25 +0600

    task update, delete and get single task api created

--- index.js
+++ index.js
@@ -33,9 +33,48 @@ async function run() {
 
 
         // task related api
+
+        app.get('/tasks', async(req, res) =>{
+            const user = req.query.user;
+            const query = {user: user};
+            const result = await taskCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/tasks/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await taskCollection.findOne(query);
+            res.send(result);
+        })
+
         app.post('/tasks', async(req, res) =>{
             const task = req.body;
             const result = await taskCollection.insertOne(task);
+            res.send(result);
+        })
+
+        app.patch('/tasks/:id', async(req, res) =>{
+            const task = req.body;
+            const id = req.params.id;
+            const filter = {_id : new ObjectId(id)};
+            const updateDoc = {
+                $set: {
+                    title: task.title,
+                    deadline: task.deadline,
+                    priority: task.priority,
+                    description: task.description
+                }
+            };
+
+            const result = await taskCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/tasks/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await taskCollection.deleteOne(query);
             res.send(result);
         })
 
